991b730597712733a19fa488e61f3808eb23839c
Michaelgal13 <41348138+Michaelgal13@users.noreply.github.com>
2021-05-27 22:14:35 -0700

    Parsing Appears to be working

---
+++ ParseTest.m
@@ -0,0 +1,11 @@
+x = 1;
+xdot = 2;
+theta = 1;
+thetadot = 2;
+
+
+tree = genTree(5);
+outPut = parseTree(tree);
+
+
+
---
+++ parseTreeHelper.m
@@ -0,0 +1,17 @@
+function outVal = parseTreeHelper(tree)
+if isleaf(tree,1)
+    outVal = string(tree.Node(1));
+    return
+else
+    curVal = string(tree.Node(1));
+    if curVal == "&+"
+        curString = "plus( ";
+    elseif curVal == "&*"
+        curString = "times( ";
+    else
+        curString = "error( ";
+    end
+    children = getchildren(tree,1);
+    curVal = strcat(curString, parseTreeHelper(tree.subtree(children(1))), ", ", parseTreeHelper(tree.subtree(children(2))),") ");
+    outVal = curVal;
+end
