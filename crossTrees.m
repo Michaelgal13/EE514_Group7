2e80edc9f93e653acfa424073a882a02e7b92269
Michaelgal13 <41348138+Michaelgal13@users.noreply.github.com>
2021-05-28 16:05:52 -0700

    updated crosstrees to take opts

--- crossTrees.m
+++ crossTrees.m
@@ -1,5 +1,18 @@
 %% Cross Trees
-function newTree = crossTrees(tree1, tree2, maxSize)
+function newTree = crossTrees(tree1, tree2, opts)
+if isempty(opts) || (size(opts,2) < 2)
+    maxSize = inf;
+else
+    a = find(opts(:) == "MaxSize");
+    if isempty(a)
+        maxSize = inf;
+    else
+        maxSize = opts(a(1)+1);
+        maxSize = str2double(maxSize);
+    end
+end
+
+
     % random select node on tree 1
     
     % cut out node of tree 1
