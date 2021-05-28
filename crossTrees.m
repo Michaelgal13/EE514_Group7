d9f91ebd9cbbe3096aadea1018df78df84d237e1
jeremysim <s.sim.jeremy@gmail.com>
2021-05-28 02:30:05 -0700

    Update crossTrees.m

--- crossTrees.m
+++ crossTrees.m
@@ -1,6 +1,10 @@
 %% Cross Trees
-function newTree = crossTrees(tree1, tree2)
+function newTree = crossTrees(tree1, tree2, maxSize)
+    % random select node on tree 1
     
+    % cut out node of tree 1
+    
+    % random select node of tree 2 (limited by tree 1 node depth + )
     
     
 end
