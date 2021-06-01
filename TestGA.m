7588eb75203a6dfe25767daab399595d1e4bbb0d
Michaelgal13 <41348138+Michaelgal13@users.noreply.github.com>
2021-06-01 04:22:39 -0700

    Fixed time and added rudimentary timeout

--- TestGA.m
+++ TestGA.m
@@ -3,18 +3,17 @@ clear all
 close all
 %%
 simulinkModel = "untitled.slx";
-simTime = 10;
-t = [0:simTime/1e4:simTime];
+t = 10;
 simFunct = "simFunct";
 costFunction = "costFunction";
 generations = 10;
-population = 5;
+population = 20;
 initDepth = 5;
-opts = ["MaxSize", 30, "MutationLoops", 2, "MutationDepth", 8,...
+opts = ["MaxSize", 40, "MutationLoops", 2, "MutationDepth", 8,...
     "NumberMax", 10, "NumberDelims", 0.5, "OperatorSeed", 0.5,...
     "EndNodeSeed", 0.5, ...
     "StatexSeed", 0.25, "StatexdotSeed", 0.25, "StatethetaSeed", 0.25,...
-    "ElitismNumber", 1, "CrossoverNumber", 1, "ReplicationNumber", 1];
+    "ElitismNumber", 1, "CrossoverNumber", floor(population/2), "ReplicationNumber", 1];
 endCond = 1;
 
 for i = 1: population
@@ -25,26 +24,30 @@ for i = 1:generations
     treeRes = zeros(size(treeList,2),1);
     for  j = 1:population
         str = parseTree(treeList(j), simFunct);
+        timre = timer('TimerFcn', ' error(''TIMEOUT!'');',...
+            'StartDelay',120);
         try
-        simOut = sim(simulinkModel, t);
-%         simOut = sim(simulinkModel, simTime);
-        treeRes(j) = feval(costFunction, simOut);
+            start(timre);
+            simOut = sim(simulinkModel, t);
+            %         simOut = sim(simulinkModel, simTime);
+            treeRes(j) = feval(costFunction, simOut);
         catch
-        fprintf("ERROR: Setting cost to inf\n");
-        treeRes(j) = inf;
+            fprintf("ERROR: Setting cost to inf\n");
+            treeRes(j) = inf;
         end
-        fprintf("Currently: %d out of %d\n", (i-1)*population + j, (generations)*population); 
+        delete(timre);
+        fprintf("Currently: %d out of %d at %s\n", (i-1)*population + j, (generations)*population, datestr(now,'HH:MM:SS.FFF'));
     end
-%     treeRes = sort(treeRes);
-%     for  j = 1:population
-%     genRes(i,j) = (treeRes(j));
-%     end
-%     plot(1:i,genRes(:,1));
-%     hold on
-%     for j = 2: population
-%         plot(1:i, genRes(:,j));
-%     end
-%     hold off
+    %     treeRes = sort(treeRes);
+    %     for  j = 1:population
+    %     genRes(i,j) = (treeRes(j));
+    %     end
+    %     plot(1:i,genRes(:,1));
+    %     hold on
+    %     for j = 2: population
+    %         plot(1:i, genRes(:,j));
+    %     end
+    %     hold off
     genRes(i) = min(treeRes);
     plot(1:i,genRes);
     xlim([1 generations]);
@@ -54,7 +57,7 @@ for i = 1:generations
     pause(0.01)
     if endCond == 1
         if min(treeRes) == 0
-            fprintf("End Condition Occured on generation %d\n", i);
+            fprintf("End Condition Occured on generation %d at %s\n",i, datestr(now,'HH:MM:SS.FFF'));
             break;
         end
     end
@@ -70,28 +73,28 @@ parseTree(result, simFunct);
 simOut = sim(simulinkModel, t);
 % simOut = sim(simulinkModel, simTime);
 y = feval(costFunction, simOut);
-
+%t2 = 1:t;
 figure(101)
 subplot(2,3,1)
-plot (t,simOut.xnew.Data(:,1)')
+plot (simOut.tout,simOut.xnew.Data(:,1)')
 xlabel ('Time')
 ylabel ('Position')
 subplot(2,3,2)
-plot (t,simOut.xnew.Data(:,2)')
+plot (simOut.tout,simOut.xnew.Data(:,2)')
 xlabel ('Time')
 ylabel ('Velocity')
 subplot(2,3,3)
-plot (t,simOut.xnew.Data(:,3)')
+plot (simOut.tout,simOut.xnew.Data(:,3)')
 xlabel ('Time')
 ylabel ('Angle')
 subplot(2,3,4)
-plot (t,simOut.xnew.Data(:,4)')
+plot (simOut.tout,simOut.xnew.Data(:,4)')
 xlabel ('Time')
 ylabel ('Angular Velocity')
 subplot(2,3,5)
-plot (t,simOut.yout)
+plot (simOut.tout,simOut.yout)
 xlabel ('Time')
 ylabel ('Acutator Effort')
 % xlabel ('Time')
 % ylabel ('Magnitude')
-% legend ({'Position', 'Velocity', 'Angle', 'Angular Velocity', 'Actuator Effort'}, 'Location', 'Best') 
+% legend ({'Position', 'Velocity', 'Angle', 'Angular Velocity', 'Actuator Effort'}, 'Location', 'Best')
