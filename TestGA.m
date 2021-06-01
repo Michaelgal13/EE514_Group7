49b01fa902b56378beee2f8dafeb23d89c1f2c21
Michaelgal13 <41348138+Michaelgal13@users.noreply.github.com>
2021-05-31 18:33:35 -0700

    Fixed GA

--- TestGA.m
+++ TestGA.m
@@ -6,15 +6,15 @@ simulinkModel = "untitled.slx";
 simTime = 5;
 simFunct = "simFunct";
 costFunction = "costFunction";
-generations = 20;
-population = 5;
+generations = 40;
+population = 10;
 initDepth = 5;
 opts = ["MaxSize", 30, "MutationLoops", 2, "MutationDepth", 8,...
     "NumberMax", 10, "NumberDelims", 0.5, "OperatorSeed", 0.5,...
     "EndNodeSeed", 0.5, ...
     "StatexSeed", 0.25, "StatexdotSeed", 0.25, "StatethetaSeed", 0.25,...
     "ElitismNumber", 1, "CrossoverNumber", 1, "ReplicationNumber", 1];
-
+endCond = 1;
 
 for i = 1: population
     treeList(i) = genTree(initDepth, opts);
@@ -23,6 +23,7 @@ end
 for i = 1:generations
     treeRes = zeros(size(treeList,2),1);
     for  j = 1:population
+        str = parseTree(treeList(j), simFunct);
         try
         simOut = sim(simulinkModel, simTime);
         treeRes(j) = feval(costFunction, simOut);
@@ -30,10 +31,30 @@ for i = 1:generations
         fprintf("ERROR: Setting cost to inf\n");
         treeRes(j) = inf;
         end
-        %str = parseTree(treeList(j), simFunct);
-        %simOut = sim(simulinkModel, simTime);
-        treeRes(j) = costFunction(simOut);
-        fprintf("Currently: %d out of %d\n", (i-1)*population + j, (generations)*population);
+        fprintf("Currently: %d out of %d\n", (i-1)*population + j, (generations)*population); 
+    end
+%     treeRes = sort(treeRes);
+%     for  j = 1:population
+%     genRes(i,j) = (treeRes(j));
+%     end
+%     plot(1:i,genRes(:,1));
+%     hold on
+%     for j = 2: population
+%         plot(1:i, genRes(:,j));
+%     end
+%     hold off
+    genRes(i) = min(treeRes);
+    plot(1:i,genRes);
+    xlim([1 generations]);
+    xlabel("Generations");
+    ylabel("Minimum Cost");
+    drawnow;
+    pause(0.01)
+    if endCond == 1
+        if min(treeRes) == 0
+            fprintf("End Condition Occured on generation %d\n", i);
+            break;
+        end
     end
     treeList = evolveGen(treeList, treeRes, opts);
     population = size(treeList,2);
