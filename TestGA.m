d328011995ec73d1c1c1070db4f73f362f7dbff2
Michaelgal13 <41348138+Michaelgal13@users.noreply.github.com>
2021-06-02 16:34:27 -0700

    Update TestGA.m

--- TestGA.m
+++ TestGA.m
@@ -4,16 +4,11 @@ close all
 %%
 simulinkModel = "untitled";
 t = 10;
-simFunct = "simFunct";
-costFunction = "costFunction";
-generations = 50;
-population = 20;
-initDepth = 5;
-opts = ["MaxSize", 40, "MutationLoops", 2, "MutationDepth", 8,...
+opts = ["MaxSize", 30, "MutationLoops", 10, "MutationDepth", 8,...
     "NumberMax", 10, "NumberDelims", 0.5, "OperatorSeed", 0.5,...
     "EndNodeSeed", 0.5, ...
     "StatexSeed", 0.25, "StatexdotSeed", 0.25, "StatethetaSeed", 0.25,...
-    "ElitismNumber", 1, "CrossoverNumber", floor(population/2), "ReplicationNumber", 1];
+    "ElitismNumber", 1, "CrossoverNumber", 8, "ReplicationNumber", 1];
 endCond = 1;
 
 for i = 1: population
@@ -23,7 +18,6 @@ end
 for i = 1:generations
     treeRes = zeros(size(treeList,2),1);
     for  j = 1:population
-        err = 0;
         str = parseTree(treeList(j), simFunct);
         timre = timer('TimerFcn', 'err = 1; set_param(simulinkModel,''SimulationCommand'',''stop'')',...
             'StartDelay',60);
@@ -43,16 +37,16 @@ for i = 1:generations
         end
         fprintf("Currently: %d out of %d at %s\n", (i-1)*population + j, (generations)*population, datestr(now,'HH:MM:SS.FFF'));
     end
-    %     treeRes = sort(treeRes);
-    %     for  j = 1:population
-    %     genRes(i,j) = (treeRes(j));
-    %     end
-    %     plot(1:i,genRes(:,1));
-    %     hold on
-    %     for j = 2: population
-    %         plot(1:i, genRes(:,j));
-    %     end
-    %     hold off
+%     treeRes = sort(treeRes);
+%     for  j = 1:population
+%     genRes(i,j) = (treeRes(j));
+%     end
+%     plot(1:i,genRes(:,1));
+%     hold on
+%     for j = 2: population
+%         plot(1:i, genRes(:,j));
+%     end
+%     hold off
     genRes(i) = min(treeRes);
     plot(1:i,genRes);
     xlim([1 generations]);
@@ -62,7 +56,7 @@ for i = 1:generations
     pause(0.01)
     if endCond == 1
         if min(treeRes) == 0
-            fprintf("End Condition Occured on generation %d at %s\n",i, datestr(now,'HH:MM:SS.FFF'));
+            fprintf("End Condition Occured on generation %d\n", i);
             break;
         end
     end
@@ -74,32 +68,85 @@ result = treeList(I);
 
 
 %%
+t = [0:simTime*1e-4:simTime];
 parseTree(result, simFunct);
 simOut = sim(simulinkModel, t);
 % simOut = sim(simulinkModel, simTime);
 y = feval(costFunction, simOut);
-%t2 = 1:t;
+
 figure(101)
 subplot(2,3,1)
+% plot (t,simOut.xnew.Data(:,1)')
+>>>>>>> main
 plot (simOut.tout,simOut.xnew.Data(:,1)')
 xlabel ('Time')
 ylabel ('Position')
 subplot(2,3,2)
+% plot (t,simOut.xnew.Data(:,2)')
+>>>>>>> main
 plot (simOut.tout,simOut.xnew.Data(:,2)')
 xlabel ('Time')
 ylabel ('Velocity')
 subplot(2,3,3)
+% plot (t,simOut.xnew.Data(:,3)')
+>>>>>>> main
 plot (simOut.tout,simOut.xnew.Data(:,3)')
 xlabel ('Time')
 ylabel ('Angle')
 subplot(2,3,4)
+% plot (t,simOut.xnew.Data(:,4)')
+>>>>>>> main
 plot (simOut.tout,simOut.xnew.Data(:,4)')
 xlabel ('Time')
 ylabel ('Angular Velocity')
 subplot(2,3,5)
+% plot (t,simOut.yout)
 plot (simOut.tout,simOut.yout)
 xlabel ('Time')
 ylabel ('Acutator Effort')
 % xlabel ('Time')
 % ylabel ('Magnitude')
 % legend ({'Position', 'Velocity', 'Angle', 'Angular Velocity', 'Actuator Effort'}, 'Location', 'Best')
+% legend ({'Position', 'Velocity', 'Angle', 'Angular Velocity', 'Actuator Effort'}, 'Location', 'Best') 
+
+
+%%
+t = [0:simTime*1e-4:simTime];
+parseTree(result, simFunct);
+simOut = sim("untitled_pi4.slx", t);
+% simOut = sim(simulinkModel, simTime);
+y = feval(costFunction, simOut);
+
+figure(201)
+subplot(2,3,1)
+% plot (t,simOut.xnew.Data(:,1)')
+plot (simOut.tout,simOut.xnew.Data(:,1)')
+xlabel ('Time')
+ylabel ('Position')
+subplot(2,3,2)
+% plot (t,simOut.xnew.Data(:,2)')
+plot (simOut.tout,simOut.xnew.Data(:,2)')
+xlabel ('Time')
+ylabel ('Velocity')
+subplot(2,3,3)
+% plot (t,simOut.xnew.Data(:,3)')
+plot (simOut.tout,simOut.xnew.Data(:,3)')
+xlabel ('Time')
+ylabel ('Angle')
+subplot(2,3,4)
+% plot (t,simOut.xnew.Data(:,4)')
+plot (simOut.tout,simOut.xnew.Data(:,4)')
+xlabel ('Time')
+ylabel ('Angular Velocity')
+subplot(2,3,5)
+% plot (t,simOut.yout)
+plot (simOut.tout,simOut.yout)
+xlabel ('Time')
+ylabel ('Acutator Effort')
+
+
+
+
+%%
+% save('test2', 'result', 'opts', 'population', 'generations', 'initDepth', 'simulinkModel')
+
