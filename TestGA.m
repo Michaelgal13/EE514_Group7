c11245f90b0b413cdaed673be10ac8362b6003c7
Michaelgal13 <41348138+Michaelgal13@users.noreply.github.com>
2021-06-02 17:03:07 -0700

    idk

--- TestGA.m
+++ TestGA.m
@@ -4,25 +4,30 @@ close all
 %%
 simulinkModel = "untitled";
 t = 10;
-opts = ["MaxSize", 30, "MutationLoops", 10, "MutationDepth", 8,...
+generations = 100;
+population = 20;
+initDepth = 5;
+simFunct = "simFunct";
+opts = ["MaxSize", 50, "MutationLoops", 10, "MutationDepth", 8,...
     "NumberMax", 10, "NumberDelims", 0.5, "OperatorSeed", 0.5,...
     "EndNodeSeed", 0.5, ...
     "StatexSeed", 0.25, "StatexdotSeed", 0.25, "StatethetaSeed", 0.25,...
     "ElitismNumber", 1, "CrossoverNumber", 8, "ReplicationNumber", 1];
-endCond = 1;
+endCond = 0;
 
 for i = 1: population
     treeList(i) = genTree(initDepth, opts);
 end
-
+tic
 for i = 1:generations
     treeRes = zeros(size(treeList,2),1);
     for  j = 1:population
+        err = 0;
         str = parseTree(treeList(j), simFunct);
-        timre = timer('TimerFcn', 'err = 1; set_param(simulinkModel,''SimulationCommand'',''stop'')',...
-            'StartDelay',60);
+        %timre = timer('TimerFcn', 'err = 1; set_param(simulinkModel,''SimulationCommand'',''stop'')',...
+        %    'StartDelay',60);
         try
-            start(timre);
+            %start(timre);
             simOut = sim(simulinkModel, t);
             %         simOut = sim(simulinkModel, simTime);
             treeRes(j) = feval(costFunction, simOut);
@@ -30,8 +35,8 @@ for i = 1:generations
             fprintf("ERROR: Setting cost to inf\n");
             treeRes(j) = inf;
         end
-        stop(timre);
-        delete(timre);
+        %stop(timre);
+        %delete(timre);
         if err ~= 0
             treeRes(j) = inf;
         end
@@ -65,10 +70,10 @@ for i = 1:generations
 end
 [~,I] = min(treeRes);
 result = treeList(I);
-
+totalTimeOut = toc;
 
 %%
-t = [0:simTime*1e-4:simTime];
+t = 10;
 parseTree(result, simFunct);
 simOut = sim(simulinkModel, t);
 % simOut = sim(simulinkModel, simTime);
@@ -77,25 +82,21 @@ y = feval(costFunction, simOut);
 figure(101)
 subplot(2,3,1)
 % plot (t,simOut.xnew.Data(:,1)')
->>>>>>> main
 plot (simOut.tout,simOut.xnew.Data(:,1)')
 xlabel ('Time')
 ylabel ('Position')
 subplot(2,3,2)
 % plot (t,simOut.xnew.Data(:,2)')
->>>>>>> main
 plot (simOut.tout,simOut.xnew.Data(:,2)')
 xlabel ('Time')
 ylabel ('Velocity')
 subplot(2,3,3)
 % plot (t,simOut.xnew.Data(:,3)')
->>>>>>> main
 plot (simOut.tout,simOut.xnew.Data(:,3)')
 xlabel ('Time')
 ylabel ('Angle')
 subplot(2,3,4)
 % plot (t,simOut.xnew.Data(:,4)')
->>>>>>> main
 plot (simOut.tout,simOut.xnew.Data(:,4)')
 xlabel ('Time')
 ylabel ('Angular Velocity')
@@ -104,6 +105,7 @@ subplot(2,3,5)
 plot (simOut.tout,simOut.yout)
 xlabel ('Time')
 ylabel ('Acutator Effort')
+sgtitle('Genetic Algorithm Results at $$\frac{\pi}{10}$$','interpreter','latex')
 % xlabel ('Time')
 % ylabel ('Magnitude')
 % legend ({'Position', 'Velocity', 'Angle', 'Angular Velocity', 'Actuator Effort'}, 'Location', 'Best')
@@ -111,7 +113,7 @@ ylabel ('Acutator Effort')
 
 
 %%
-t = [0:simTime*1e-4:simTime];
+t = 10;
 parseTree(result, simFunct);
 simOut = sim("untitled_pi4.slx", t);
 % simOut = sim(simulinkModel, simTime);
@@ -144,9 +146,10 @@ plot (simOut.tout,simOut.yout)
 xlabel ('Time')
 ylabel ('Acutator Effort')
 
+sgtitle('Genetic Algorithm Results at $$\frac{\pi}{4}$$','interpreter','latex')
 
 
 
 %%
-% save('test2', 'result', 'opts', 'population', 'generations', 'initDepth', 'simulinkModel')
-
+save('test2', 'result', 'opts', 'population', 'generations', 'initDepth', 'simulinkModel')
+parseTree(result, "ResultsV2");
